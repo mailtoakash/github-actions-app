af57943fcc0be6f5e5f2d65978a19884caa00f6c
Akash <mailtoakash100@gmail.com>
2026-05-20 14:25:25 +0000

    application test deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("vanakam");
+  res.send("The Github Action Is Properly Working");
 });
 
 app.listen(3000, () => {
